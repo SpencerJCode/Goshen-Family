5cbabb59cdeffdfb3e9c53e79aab2f77c65c8e02
Spencer Johnson <123844048+SpencerJCode@users.noreply.github.com>
2024-07-20 17:21:07 -0400

    Website complete

---
+++ software/script.js
@@ -0,0 +1,7 @@
+function copyText() {
+     // Copy the text inside the text field
+    navigator.clipboard.writeText("software@goshen.family");
+  
+    // Alert the copied text
+    alert("Copied the text: software@goshen.family");
+  }
